acfeaa20e60a09225e2e98860a8547bda13922d7
Carlin Mitchell <carlinmi@umn.edu>
2023-08-03 11:28:24 -0500

    “finish colors section”

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,19 @@
 import Element from "./Element";
 
 import Card from "./Card";
+import Colors from "./Colors";
 
 const Content = () => {
   const cardsHeader = Element("h2", { innerText: `Cards` });
-  const cardComponent = Card();
+  const colorsHeader = Element("h2", { innerText: `Colors` });
 
   const content = Element("div", { className: `content` }, [
+    colorsHeader,
+    Colors(),
+    Element("hr", { className: `bg-black` }),
     cardsHeader,
-    cardComponent,
+    Card(),
+    Element("hr", { className: `bg-black` }),
   ]);
   return content;
 };
