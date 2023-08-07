5ba2cb920c3193001f90f5b5ca1266706972793b
Carlin Mitchell <carlinmi@umn.edu>
2023-08-07 08:25:24 -0500

    “functions”

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -13,12 +13,23 @@ const Buttons = () => {
     button("btn-info" + sharedClasses, "btn-info"),
     button("btn-green" + sharedClasses, "btn-green"),
     button("btn-purple" + sharedClasses, "btn-purple"),
+
     button("btn-outline-primary" + sharedClasses, "btn-outline-primary"),
     button("btn-outline-secondary" + sharedClasses, "btn-outline-secondary"),
     button("btn-outline-error" + sharedClasses, "btn-outline-error"),
     button("btn-outline-info" + sharedClasses, "btn-outline-info"),
     button("btn-outline-green" + sharedClasses, "btn-outline-green"),
     button("btn-outline-purple" + sharedClasses, "btn-outline-purple"),
+
+    button("btn-compliment-primary" + sharedClasses, "btn-compliment-primary"),
+    button(
+      "btn-compliment-secondary" + sharedClasses,
+      "btn-compliment-secondary"
+    ),
+    button("btn-compliment-error" + sharedClasses, "btn-compliment-error"),
+    button("btn-compliment-info" + sharedClasses, "btn-compliment-info"),
+    button("btn-compliment-green" + sharedClasses, "btn-compliment-green"),
+    button("btn-compliment-purple" + sharedClasses, "btn-compliment-purple"),
   ];
 
   buttonElems.forEach((elem) => buttonsContainer.appendChild(elem));
